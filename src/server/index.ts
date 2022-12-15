50358b189c35bbb6d92c554d0bf2d90b08d0c2ef
joaquinns <joaquindevv96@gmail.com>
2022-12-15 10:47:27 -0400

    created server redirect

---
+++ src/server/index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from 'express'
+// security
+import cors from 'cors'
+import helmet from 'helmet'
+//routes
+import router from '../routes'
+
+const server: Express = express()
+
+// define server to use '/api' and routes
+server.use('/api', router)
+
+// security config
+server.use(helmet())
+server.use(cors())
+
+// content-type config
+server.use(express.urlencoded({ extended: true, limit: '50mb' }))
+server.use(express.json({ limit: '50mb' }))
+
+server.get('/', (req: Request, res: Response) => {
+  res.redirect('/api')
+})
+
+export default server
