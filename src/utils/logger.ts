65c7c4d1614e1d7a4cac2db11d0ff687478f7207
joaquinns <joaquindevv96@gmail.com>
2022-12-15 10:45:08 -0400

    created custom logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+export const logSuccess = (msg: string) => {
+  console.log(`Success: ${msg}`)
+}
+
+export const logError = (msg: string) => {
+  console.log(`Error: ${msg}`)
+}
+
+export const logInfo = (msg: string) => {
+  console.log(`Info: ${msg}`)
+}
+
+export const logWarning = (msg: string) => {
+  console.log(`Warning: ${msg}`)
+}
