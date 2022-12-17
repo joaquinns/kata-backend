ba93d4764ae9ad5cd26e1fb45a56a3bc40cecf82
joaquinns <joaquindevv96@gmail.com>
2022-12-16 22:52:30 -0400

    added user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express'
 import exampleRoute from './example.router'
+import usersRoute from './user.router'
 
 let server: Express = express()
 
@@ -11,5 +12,6 @@ rootRouter.get('/', (_req: Request, res: Response) => {
 
 server.use('/', rootRouter) // localhost:8000/api/
 server.use('/example', exampleRoute) // localhost:8000/api/example
+server.use('/users', usersRoute) // localhost:8000/api/example
 
 export default server
