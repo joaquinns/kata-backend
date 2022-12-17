656a94382449bb19bc203437841cc12363e710dd
joaquinns <joaquindevv96@gmail.com>
2022-12-16 22:52:10 -0400

    added user interface

--- src/controllers/interfaces/index.ts
+++ src/controllers/interfaces/index.ts
@@ -1,5 +1,11 @@
-import { BasicResponse } from '../types'
+import { BasicResponse, UserType } from '../types'
 
 export interface Example {
   getMessage: (name?: string) => Promise<BasicResponse>
+}
+
+export interface User {
+  getUsers: (id?: string) => Promise<UserType | UserType[] | undefined>
+  deleteUser: (id: string) => Promise<any>
+  createUser: (user: UserType) => Promise<UserType>
 }
