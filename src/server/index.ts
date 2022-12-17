00f3284ae58ea04f0c060ad6ea6840124f1adf19
joaquinns <joaquindevv96@gmail.com>
2022-12-16 22:52:56 -0400

    sort for read parsed json at first

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,31 @@
+import dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDoc from '../../public/swagger.json'
 // security
 import cors from 'cors'
 import helmet from 'helmet'
 //routes
+import mongoose from 'mongoose'
 import router from '../routes'
+import { logInfo } from '../utils/logger'
+
+dotenv.config()
 
 const server: Express = express()
+
+// content-type config
+server.use(express.urlencoded({ extended: true, limit: '50mb' }))
+server.use(express.json({ limit: '50mb' }))
+
+// swagger docs config route
+server.use(
+  '/docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDoc, {
+    explorer: true
+  })
+)
 
 // define server to use '/api' and routes
 server.use('/api', router)
@@ -13,13 +33,13 @@ server.use('/api', router)
 // static server
 server.use(express.static('/public'))
 
+// connect to db
+mongoose.connect(process.env.DB_HOST as string)
+logInfo(process.env.DB_HOST as string)
+
 // security config
 server.use(helmet())
 server.use(cors())
-
-// content-type config
-server.use(express.urlencoded({ extended: true, limit: '50mb' }))
-server.use(express.json({ limit: '50mb' }))
 
 server.get('/', (req: Request, res: Response) => {
   res.redirect('/api')
