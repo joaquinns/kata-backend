fb28faedbbc49308db6ca66f988107204458a449
joaquinns <joaquindevv96@gmail.com>
2022-12-22 14:02:23 -0400

    created auth controller interface

--- src/controllers/interfaces/index.ts
+++ src/controllers/interfaces/index.ts
@@ -4,8 +4,14 @@ export interface Example {
   getMessage: (name?: string) => Promise<BasicResponse>
 }
 
-export interface User {
+export interface IUserController {
   getUsers: (id?: string) => Promise<UserType | UserType[] | undefined>
   deleteUser: (id: string) => Promise<any>
   createUser: (user: UserType) => Promise<UserType>
+  updateUser: (id: string, user: any) => Promise<any>
+}
+
+export interface IAuthController {
+  registerUser: (user: UserType) => Promise<any>
+  loginUser: (auth: any) => Promise<any>
 }
