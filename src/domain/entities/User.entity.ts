8155e0e92d1fb54dbfe6410609c3df5ef4dfc8f3
joaquinns <joaquindevv96@gmail.com>
2022-12-22 14:01:45 -0400

    added user type to model

--- src/domain/entities/User.entity.ts
+++ src/domain/entities/User.entity.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
+import { UserType } from '../../controllers/types/index'
 
 export const userEntity = () => {
-  let userSchema = new mongoose.Schema({
-    name: String,
-    email: String
+  let userSchema = new mongoose.Schema<UserType>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
   })
 
-  return mongoose.models.users || mongoose.model('users', userSchema)
+  return mongoose.models.users || mongoose.model<UserType>('users', userSchema)
 }
