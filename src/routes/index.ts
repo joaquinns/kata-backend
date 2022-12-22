2cd1f68cf716f8c8b76d9cb604976b5d286610d3
joaquinns <joaquindevv96@gmail.com>
2022-12-22 14:04:39 -0400

    added auth router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express'
+import authRoute from './auth.router'
 import exampleRoute from './example.router'
 import usersRoute from './user.router'
 
@@ -12,6 +13,7 @@ rootRouter.get('/', (_req: Request, res: Response) => {
 
 server.use('/', rootRouter) // localhost:8000/api/
 server.use('/example', exampleRoute) // localhost:8000/api/example
-server.use('/users', usersRoute) // localhost:8000/api/example
+server.use('/auth', authRoute) // localhost:8000/api/auth
+server.use('/users', usersRoute) // localhost:8000/api/users
 
 export default server
