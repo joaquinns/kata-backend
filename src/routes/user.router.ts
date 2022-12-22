da2b38b19fc182a53677d8f9c05c6903b32a5187
joaquinns <joaquindevv96@gmail.com>
2022-12-22 14:00:02 -0400

    added req body to create route

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import express, { Request, Response } from 'express'
 
 import { UserController } from '../controllers/user.controller'
@@ -17,17 +18,30 @@ usersRoute
   .delete(async (req: Request, res: Response) => {
     let userId = req.query.id as string
     logInfo(`Query param: ${userId}`)
-    const response = userController.deleteUser(userId)
+    const response = await userController.deleteUser(userId)
+    return res.send(response)
+  })
+  .put(async (req: Request, res: Response) => {
+    logInfo(req.body)
+    const id = req.query.id as string
+    const updateUser = {
+      name: req?.body?.name,
+      email: req?.body?.email
+    }
+    const response = await userController.updateUser(id, updateUser)
     return res.send(response)
   })
   .post(async (req: Request, res: Response) => {
     logInfo(req.body)
+    const { name, email, password } = req.body
+    const passwordHash = bcrypt.hashSync(password, 10)
     const newUser = {
-      name: req.body.name as string,
-      email: req.body.email as string
+      name,
+      email,
+      password: passwordHash
     }
-    const response = userController.createUser(newUser)
-    return res.send(response)
+    const response = await userController.createUser(newUser)
+    return res.status(201).json(response)
   })
 
 export default usersRoute
