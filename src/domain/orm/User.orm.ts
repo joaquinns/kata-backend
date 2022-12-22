5fbd5788c543ce34006ebcc078f139139ded9dc0
joaquinns <joaquindevv96@gmail.com>
2022-12-22 14:00:40 -0400

    added loginUser method

--- src/domain/orm/User.orm.ts
+++ src/domain/orm/User.orm.ts
@@ -1,6 +1,14 @@
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 import { UserType } from '../../controllers/types'
 import { logError } from '../../utils/logger'
 import { userEntity } from '../entities/User.entity'
+import { IAuth } from '../interfaces/auth.interface'
+
+dotenv.config()
+
+const jwtSecret = process.env.JWT_SECRET as string
 
 const userModel = userEntity()
 
@@ -29,10 +37,55 @@ export const createUser = async (user: UserType): Promise<any> => {
   }
 }
 
+export const updateUser = async (id: string, user: any): Promise<any> => {
+  try {
+    return await userModel.findByIdAndUpdate(id, user)
+  } catch (error) {
+    logError(`[ORM ERROR UPDATING USER]: ${error}`)
+  }
+}
+
 export const deleteUser = async (id: string): Promise<any> => {
   try {
     return await userModel.deleteOne({ _id: id })
   } catch (error) {
     logError(`[ORM ERROR DELETING USER]: ${error}`)
   }
+}
+
+export const loginUser = async (
+  auth: IAuth
+): Promise<{ user: UserType | undefined; token: string } | undefined> => {
+  try {
+    let userFound: UserType | undefined = undefined
+    await userModel
+      .findOne({ email: auth.email })
+      .then((user: UserType) => {
+        userFound = user
+      })
+      .catch((err: any) => console.log(err))
+
+    const validPassword = bcrypt.compareSync(auth.password, userFound!.password)
+
+    if (!validPassword) {
+      console.error(`[Error Authentication in ORM]: Not valid password`)
+
+      throw new Error('[Error Authentication in ORM]: Not valid password')
+    }
+
+    // create jwt token
+    const jwtToken = jwt.sign(
+      // Todo to search how to add the object id from mongodb
+      { id: userFound!.id, email: userFound!.email },
+      jwtSecret,
+      {
+        expiresIn: 86400
+      }
+    )
+
+    return {
+      user: userFound,
+      token: jwtToken
+    }
+  } catch (error) {}
 }
